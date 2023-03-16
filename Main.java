df0c3167c6bc90984a49a4456d49254601bbeedb
rustam <rusitama@gmail.com>
2023-03-16 16:44:34 +0500

    Create class FileManager

--- Main.java
+++ Main.java
@@ -1,41 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.*;
-
 /**
  * Main
  */
 public class Main {
 
     public static void main(String[] args) {
-        loadFromFile("toys.txt"); 
-        writeToFile("toys.txt", "hello");       
+        FileManager.loadFromFile("toys.txt"); 
+        FileManager.writeToFile("toys.txt", "hello");       
     }
 
-    public static String loadFromFile(String filename){
-        File file = new File(filename);
-        StringBuilder result = new StringBuilder();
-        if (file.exists()){
-            try (BufferedReader reader = new BufferedReader(new FileReader(filename))){
-                String line;
-                while ((line = reader.readLine()) != null){
-                    result.append(line);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }            
-        }
-        return result.toString();
-    }
-
-    public static void writeToFile(String filename, String data) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))){
-            writer.write(data);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+    
 }
